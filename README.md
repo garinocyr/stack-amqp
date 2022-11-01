77e32e57c5ceb20551f8c064be6af0aab84bc64d
garinocyr <cgarino@yahoo.fr>
2022-11-01 13:40:59 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+Docker stack for AMQP purpose
+- Uses rabbitmq as AMQP broker (see https://www.rabbitmq.com/)
+
+### Usage
+
+* Start the stack
+  * From local git repository
+
+```console
+developer@host:~/.../stack-amqp$ docker-compose up -d
+```
+
+* Stop the stack
+  * From local git repository
+
+```console
+developer@host:~/.../stack-amqp$ docker-compose down
+```
+
+### Notes
+
---
+++ scripts/encode-password.sh
@@ -0,0 +1,14 @@
+#
+# From https://stackoverflow.com/questions/41306350/how-to-generate-password-hash-for-rabbitmq-management-http-api
+#
+#!/bin/bash
+
+function encode_password() {
+  SALT=$(od -A n -t x -N 4 /dev/urandom)
+  PASS=$SALT$(echo -n $1 | xxd -ps | tr -d '\n' | tr -d ' ')
+  PASS=$(echo -n $PASS | xxd -r -p | sha256sum | head -c 128)
+  PASS=$(echo -n $SALT$PASS | xxd -r -p | base64 | tr -d '\n')
+  echo $PASS
+}
+
+encode_password
